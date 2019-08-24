3e68887c8a4eeb0fe328773a04ccf2554c8abc99
Emil <edevantie@gmail.com>
2019-08-25 00:45:50 +0200

    Add Node.js server

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const app = require('./app');
+
+//define port to be used
+const port = process.env.PORT || 3100;
+app.set('port', port);
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log("Server running")
+});
+
+module.exports = server;
