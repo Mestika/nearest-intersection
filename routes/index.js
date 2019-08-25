a5b0bcf0f59d81ea4be4cc7869083996441a320d
Emil <edevantie@gmail.com>
2019-08-25 12:38:12 +0200

    Add comment

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const IntersectionCtrl = require('../controllers/intersections');
 const router = express.Router();
 
+/**
+ * ADD ADDITIONAL ROUTES HERE BOTH FOR APP AND FOR API
+ */
 router.get('/api/nearest/:lat/:lon', IntersectionCtrl.findNearest);
 
 module.exports = router;
