b6e6025a5bf0ceb90602f58e70cf5f66df22266e
Emil <edevantie@gmail.com>
2019-08-25 12:38:28 +0200

    Add general comments

--- app.js
+++ app.js
@@ -20,13 +20,18 @@ app.use((req, res, next) => {
   next();
 });
 
-
+/**
+ * CAPTURE 404 ERRORS
+ */
 app.use((req, res, next) => {
   const error = new Error("Not found");
   error.status = 404;
   next(error);
 });
 
+/**
+ * GENERAL FUNCTION FOR CAPTURING ERRORS
+ */
 app.use((error, req, res, next) => {
   res.status(error.status || 500);
   res.json({
