050a19fe2ca133bb49b05b3448b9f1d852c54080
Emil <edevantie@gmail.com>
2019-08-25 11:41:18 +0200

    Sanity check

--- controllers/intersections.js
+++ controllers/intersections.js
@@ -2,18 +2,25 @@ const calculations = require('../helpers/calculations');
 
 class IntersectionController {
 
-  findNearest(req, res){
+  findNearest(req, res) {
+
     const lat = parseFloat(req.params.lat);
     const lon = parseFloat(req.params.lon);
+    if (isNaN(lat) || isNaN(lon)) {
+      const err = new Error();
+      err.status = 400;
+      err.message = 'Latitude or Longtitude is not a valid number'
+      throw err;
+    }
+
     let result = calculations(lat, lon);
     return res.status(200).send({
       success: 'true',
       message: 'Nearest Intersection Found',
       result: result,
-      });
+    });
   }
 }
 
 const IntersectionCtrl = new IntersectionController();
 module.exports = IntersectionCtrl;
-
